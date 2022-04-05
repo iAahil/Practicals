43d51b59ee6007197f40049fe9e42833738f0e1b
aahil <ismail.inaz@my.jcu.edu.au>
2022-04-05 18:46:46 +0500

    Refactor broken score program to use functions

--- prac_3/broken_score.py
+++ prac_3/broken_score.py
@@ -2,14 +2,25 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+def main():
 
-score = float(input("Enter score: "))
-if 90 <= score <= 100:
-    print("Your score is Excellent!")
-elif 50 <= score < 90:
-    print("Your score is Passable")
-elif 50 > score >= 0:
-    print("Your score is Bad")
-else:
-    print("Your score is Invalid")
+    score = float(input("Enter score: "))
+    return_result(score)
+    print(return_result(score))
+
+
+def return_result(score):
+    if 90 <= score <= 100:
+        return "Your score is excellent"
+    elif 50 <= score < 90:
+        return "Your score is passable"
+    elif 50 > score >= 0:
+        return "Your score is bad"
+    else:
+        return "Your score is invalid"
+
+main()
+
+print(random.randint(0,100))
 

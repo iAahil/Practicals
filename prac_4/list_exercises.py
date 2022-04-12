152cbbeee322ae37d0bc054f9590da89ed0bbaf4
aahil <ismail.inaz@my.jcu.edu.au>
2022-04-12 17:55:32 +0500

    Adds program that grants access based on username

--- prac_4/list_exercises.py
+++ prac_4/list_exercises.py
@@ -8,3 +8,11 @@ print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the number is {}".format(average))
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter Username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
